cfa859a41a42d79098cf4f0a168869d573471a63
Pkphakiso <phakiso.ngwevu@gmail.com>
2024-01-25 14:22:11 +0200

    one book added

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -13,11 +13,22 @@ class BookController extends Controller
     public function index(Request $request)
     {
         $title = $request->input("title");
+        $filter = $request->input('filter', "");
 
         $books = Book::when($title , function ($query  , $title){
             return $query->title($title);
-        })->get();
+        });
+
+        $books = match($filter){
+            'popular_last_month' => $books->popularLastMonth(),
+            'popular_last_6months' => $books->popularLast6Months(),
+            'highest_rated_last_month' => $books->highestRatedLastMonth(),
+            'highest_rated_last_6months' => $books->highestRatedLast6Months(),
+            default => $books->latest()
+        };
+        $books = $books->get();
         // $books = Book::all();
+       // return error_log($books);
         return view("books.index", ["books" => $books]);
         //
     }
@@ -41,8 +52,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
+        return view(
+            "books.show", 
+            [
+                "book"=> $book->load([
+                    "reviews" => fn ($query) => $query->latest()
+                ])
+            ]
+        );
         //
     }
 
