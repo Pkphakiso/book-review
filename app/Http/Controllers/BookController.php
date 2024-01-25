48efcf83809d1fd89f76d783763adac754569615
Pkphakiso <phakiso.ngwevu@gmail.com>
2024-01-25 15:21:50 +0200

    cache

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -26,9 +26,11 @@ class BookController extends Controller
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
             default => $books->latest()
         };
-        $books = $books->get();
-        // $books = Book::all();
-       // return error_log($books);
+
+        // $books = $books->get();
+        $cacheKey = 'books:'. $filter . ':' . $title;
+        $books = cache()->remember($cacheKey, 3600, fn()=> $books->get());
+      
         return view("books.index", ["books" => $books]);
         //
     }
